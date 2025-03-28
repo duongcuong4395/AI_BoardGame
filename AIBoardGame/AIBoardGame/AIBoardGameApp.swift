1c615e09e729c7498922da90183faa3919c9f48d
duongcuong4395 <dangduongcuong@gmail.com>
2025-03-28 17:47:25 +0700

    init

---
+++ AIBoardGame/AIBoardGame/AIBoardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIBoardGameApp.swift
+//  AIBoardGame
+//
+//  Created by Macbook on 28/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AIBoardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
